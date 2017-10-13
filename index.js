dc8eb090fa67f3d8e82ec46645f3726f63fdbf5d
yejiaxing <491713443@qq.com>
2017-10-13 14:22:34 +0800

    js

---
+++ index.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(".zhuti1-div1 img").mousemove(function(){
+        $(".zhuti1-div1 img").attr("src","img/wotupian.gif");
+    }).mouseout(function(){
+        $(".zhuti1-div1 img").attr("src","img/wodetupian2.jpg");
+    });
+});/**
+ * Created by Administrator on 2016/12/21.
+ */
